dcb87b4f8052453a5276427fdb4fea729ba903f0
Hatim Gheewala <gheewala.hatim08@gmail.com>
2022-10-23 20:30:43 +0300

    Add related entity and display on page

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,13 +1,19 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ContosoUniversity.Models
 {
     public class Student
     {
         public int ID { get; set; }
+
+        [DisplayName("Last Name")]
         public string LastName { get; set; } = string.Empty;
+
+        [DisplayName("First Mid Name")]
         public string FirstMidName { get; set; } = string.Empty;
 
+        [DisplayName("Enrollment Date")]
         [DataType(DataType.DateTime)]
         public DateTime EnrollmentDate { get; set; }
         public ICollection<Enrollment> Enrollments { get; set; }
