ae203697decca7268777ec28b5060cab8018dda6
Hatim Gheewala <h.gheewala@zenithr.com>
2022-10-24 16:11:17 +0300

    Add course, instructors, departments pages

--- Models/StudentVM.cs
+++ Models/StudentVM.cs
@@ -1,10 +1,20 @@
-﻿namespace ContosoUniversity.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
 {
     public class StudentVM
     {
         public int ID { get; set; }
+
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
+
+        [DisplayName("First Mid Name")]
         public string FirstMidName  { get; set; }
+
+        [DisplayName("Enrollment Date")]
+        [DataType(DataType.DateTime)]
         public DateTime EnrollmentDate  { get; set; }
     }
 }
