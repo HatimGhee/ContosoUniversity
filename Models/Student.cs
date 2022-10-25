b82d91e99268b131bdc2616930555bfb2adc3e29
Hatim Gheewala <h.gheewala@zenithr.com>
2022-10-25 12:39:01 +0300

    Add validations and update column names

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
@@ -7,15 +8,24 @@ namespace ContosoUniversity.Models
     {
         public int ID { get; set; }
 
+        [Required]
+        [StringLength(50)]
         [DisplayName("Last Name")]
         public string LastName { get; set; } = string.Empty;
 
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 char")]
+        [Column("FirstName")]
         [DisplayName("First Mid Name")]
         public string FirstMidName { get; set; } = string.Empty;
 
         [DisplayName("Enrollment Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [DataType(DataType.DateTime)]
         public DateTime EnrollmentDate { get; set; }
+
+        [DisplayName("Full Name")]
+        public string FullName { get { return LastName + ", " + FirstMidName; } }
         public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
--- Models/StudentVM.cs
+++ Models/StudentVM.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models
 {
@@ -7,13 +8,19 @@ namespace ContosoUniversity.Models
     {
         public int ID { get; set; }
 
+        [Required]
+        [StringLength(50)]
         [DisplayName("Last Name")]
         public string LastName { get; set; }
 
+        [Required]
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 50 char")]
+        [Column("FirstName")]
         [DisplayName("First Mid Name")]
         public string FirstMidName  { get; set; }
 
         [DisplayName("Enrollment Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [DataType(DataType.DateTime)]
         public DateTime EnrollmentDate  { get; set; }
     }
