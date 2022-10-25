c0de441ad82e7edbf1e32e3108af64c25ba36e61
Hatim Gheewala <h.gheewala@zenithr.com>
2022-10-25 12:54:58 +0300

    Add instructor and office assignment entities

---
+++ Models/Instructor.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Instructor
+    {
+        public int ID { get; set; }
+
+        [Required, DisplayName("Last Name"), StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required, Column("FirstName"), DisplayName("First Name"), StringLength(50)]
+        public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date), DisplayName("Hire Date"), DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime HireDate { get; set; }
+
+        [DisplayName("Full Name")]
+        public string FullName { get { return LastName + ", " + FirstMidName; } }
+
+        public ICollection<Course> Courses { get; set; }
+        public OfficeAssignment OfficeAssignment { get; set; }
+    }
+}
---
+++ Models/OfficeAssignment.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models
+{
+    public class OfficeAssignment
+    {
+        public int ID { get; set; }
+
+        [StringLength(50), DisplayName("Office Location")]
+        public string Location { get; set; }
+        public Instructor Instructor { get; set; }
+    }
+}
